af1ebab4dc6cd2b988212ef252946b05378e7acc
Farzana Wani <farzana@imobisoft.co.uk>
2026-01-09 14:50:01 +0530

    refactor contact-us

--- src/components/contactUs/contactus.jsx
+++ src/components/contactUs/contactus.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function contactus() {
-  return <div>contactus</div>;
+  return (
+    <div>
+      contactus
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
+        dolores quae sed.
+      </p>
+      <button>contact</button>
+    </div>
+  );
 }
