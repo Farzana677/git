9515e68c13094d578adb33f5ff5cb308405723a7
Farzana Wani <farzana@imobisoft.co.uk>
2026-01-09 14:56:10 +0530

    fix: resolved the commets

--- src/components/contactUs/contactus.jsx
+++ src/components/contactUs/contactus.jsx
@@ -4,10 +4,6 @@ export default function contactus() {
   return (
     <div>
       contactus
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
-        dolores quae sed.
-      </p>
       <button>contact</button>
     </div>
   );
